19dc48aaa411b0e014061fbc4c68bfd6d5402b2a
Julio Quinteros <jquinter@gmail.com>
2019-12-10 21:23:08 -0300

    Working version with data being loaded from json file

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,35 @@
 <template>
   <div id="app">
-    <vue-json-pretty
-        v-if="renderOK"
-        :path="path"
-        :data="json"
-        :deep="deep"
-        :show-double-quotes="showDoubleQuotes"
-        :highlight-mouseover-node="highlightMouseoverNode"
-        :highlight-selected-node="highlightSelectedNode"
-        :show-length="showLength"
-        :show-line="showLine"
-        :select-on-click-node="selectOnClickNode"
-        :path-selectable="((path, data) => typeof data !== 'number')"
-        :selectable-type="selectableType"
-        :show-select-controller="showSelectController"
-        v-model="value"
-        @click="handleClick(...arguments, 'complexTree')">
-    </vue-json-pretty>
-    <h3>v-model:</h3>
-    <div>{{value}}</div>
-    <h3>Current Click:</h3>
-    <div>path: {{itemPath}}</div>
-    <div>data: <pre>{{itemData}}</pre></div>
+    <div v-for="(item, index) in info" :key="index">
+      <span>{{index+1}}</span>{{item}}
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import VueJsonPretty from 'vue-json-pretty'
 
 export default {
   name: 'axis',
 
-  components: {
-    VueJsonPretty
-  },
-
   data () {
     return {
-      info: null,
-      renderOK: true,
-      value: 'res.error',
-      selectableType: 'multiple',
-      showSelectController: true,
-      showLength: true,
-      showLine: true,
-      showDoubleQuotes: true,
-      highlightMouseoverNode: true,
-      highlightSelectedNode: true,
-      selectOnClickNode: true,
-      path: 'res',
-      deep: 3,
-      itemData: {},
-      itemPath: ''
+      info: null
     }
   },
 
   created () {
+    // var dataUrl = '/data/roles_detailed_stringify.json'
+    var dataUrl = '/data/roles_detailed.json'
+
+    //  .get('https://api.coindesk.com/v1/bpi/currentprice.json')
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.info = JSON.stringify(response)))
-  },
-
-  watch: {
-    selectableType (newVal) {
-      this.renderOK = false
-      if (newVal === 'single') {
-        this.value = 'res.error'
-      } else if (newVal === 'multiple') {
-        this.value = ['res.error', 'res.data[0].title']
-      }
-      // 重新渲染, 因为2中情况的v-model格式不同
-      this.$nextTick(() => {
-        this.renderOK = true
-      })
-    }
-  },
-  computed: {
-    json () {
-      try {
-        return JSON.parse(this.info)
-      } catch (err) {
-        return this.cache || this.data
-      }
-    }
-  },
-
-  methods: {
-    handleClick (path, data, treeName = '') {
-      this.itemPath = path
-      this.itemData = !data ? data + '' : data // 处理 data = null 的情况
-    },
-    handleChange (newVal, oldVal) {
-    }
+      .get(dataUrl)
+      .then(
+        // response => (console.log(JSON.parse(response.data))),
+        response => (this.info = response.data.slice(0, 10)),
+        error => console.error(error)
+      )
   }
 }
 </script>
