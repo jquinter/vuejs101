2f4eeacd3461c8e651568fa69bedbec086264785
Julio Quinteros <jquinter@gmail.com>
2019-12-20 17:19:55 -0300

    removeConsolePlugin

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
+const removeConsolePlugin = []
+if(process.env.NODE_ENV === 'production') {
+  removeConsolePlugin.push("transform-remove-console")
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: [
-    ['@babel/plugin-proposal-object-rest-spread', { 'loose': true, 'useBuiltIns': true }]
+    ['@babel/plugin-proposal-object-rest-spread', { 'loose': true, 'useBuiltIns': true }],
+    removeConsolePlugin
   ]
 }
