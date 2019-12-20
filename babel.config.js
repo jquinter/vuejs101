12aa3ec20d97ec96fb75660f8bea01dfceaa8634
Julio Quinteros <jquinter@gmail.com>
2019-12-20 18:38:46 -0300

    Fix babel.config.js plugin syntax

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
-const removeConsolePlugin = []
+const plugins = []
+plugins.push(['@babel/plugin-proposal-object-rest-spread', { 'loose': true, 'useBuiltIns': true }])
 if(process.env.NODE_ENV === 'production') {
-  removeConsolePlugin.push("transform-remove-console")
+  plugins.push("transform-remove-console")
 }
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: [
-    ['@babel/plugin-proposal-object-rest-spread', { 'loose': true, 'useBuiltIns': true }],
-    removeConsolePlugin
-  ]
+  plugins: plugins
 }
