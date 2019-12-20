9aae566f9de88b276a231e277acae93b03c07366
Julio Quinteros <jquinter@gmail.com>
2019-12-20 17:37:47 -0300

    compression-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+// Add this line:
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
+  chainWebpack(config) {
+    config.plugins.delete('prefetch');
+
+    // and this line
+    config.plugin('CompressionPlugin').use(CompressionPlugin);
+  },
   configureWebpack: {
     devtool: 'source-map'
   },
