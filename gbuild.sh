4495be8c1956ec2e80715b8355245bbf5f3b0ca8
Julio Quinteros <jquinter@gmail.com>
2019-12-26 08:23:12 -0300

    Deployment script

--- gbuild.sh
+++ gbuild.sh
@@ -1,6 +1,13 @@
-docker push us.gcr.io/dev-site-jq/iamodeler:beta
-docker build --no-cache . -t iamodeler
-docker tag iamodeler us.gcr.io/dev-site-jq/iamodeler:bet
-docker push us.gcr.io/dev-site-jq/iamodeler:beta
-#gcloud builds submit --tag us.gcr.io/dev-site-jq/iamodeler:beta .
-gcloud beta run deploy iamodeler --image us.gcr.io/dev-site-jq/iamodeler:beta --platform managed --region us-central1
+#!/bin/bash
+usage(){
+	echo "Usage: $0 APPNAME PROJECT_ID DEPLOY_REGION"
+	exit 1
+}
+[[ $# -ne 3 ]] && usage
+
+docker push us.gcr.io/$2/$1:beta
+docker build --no-cache . -t $1
+docker tag $1 us.gcr.io/$2/$1:beta
+docker push us.gcr.io/$2/$1:beta
+#gcloud builds submit --tag us.gcr.io/$2/$1:beta .
+gcloud beta run deploy $1 --image us.gcr.io/$2/$1:beta --platform managed --region $3
