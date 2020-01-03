381f57c62dbfe9e2d3c4ffeba7bba59d7d8f47ca
Julio Quinteros <jquinter@gmail.com>
2020-01-02 23:26:06 -0300

    Improved vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,22 @@
-// Add this line:
 const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   chainWebpack (config) {
     config.plugins.delete('prefetch')
-    config.optimization.minimize(true)
-
-    // and this line
     config.plugin('CompressionPlugin').use(CompressionPlugin)
+    config.optimization.minimize(true)
+    config.optimization.minimizer('terser').tap((args) => {
+      args[0].terserOptions.compress.drop_console = true
+      return args
+    })
   },
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src')
+      }
+    }
   },
   transpileDependencies: [
     /(\/|\\)vuetify(\/|\\)/
