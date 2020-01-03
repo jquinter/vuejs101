70eefa79a58b9c56f5e6979c0fe55e6ce9e70faf
Julio Quinteros <jquinter@gmail.com>
2020-01-02 23:25:53 -0300

    Removing babel-plugin-transform-remove-console

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,8 @@
+require('@babel/polyfill')
 const isProd = process.env.NODE_ENV === 'production'
 const plugins = []
 plugins.push(['@babel/plugin-proposal-object-rest-spread', { 'loose': true, 'useBuiltIns': true }])
-if (isProd) {
-  plugins.push('transform-remove-console')
-}
 plugins.push('@babel/plugin-transform-regenerator')
-// if(process.env.NODE_ENV === 'production') {
-//   plugins.push("transform-remove-console")
-// }
 module.exports = {
   presets: [
     // '@vue/cli-plugin-babel/preset',
