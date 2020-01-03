24f5be66a5bb29abfba3153fc9e9393f87223f32
Julio Quinteros <jquinter@gmail.com>
2020-01-03 09:31:29 -0300

    Trying output in terser

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
     config.optimization.minimize(true)
     config.optimization.minimizer('terser').tap((args) => {
       args[0].terserOptions.compress.drop_console = true
+      args[0].terserOptions.output = { comments: false, beautify: false }
       return args
     })
   },
